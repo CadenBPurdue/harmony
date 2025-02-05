c4e3aae56d91085fc0cf5e957f4568dfa34ad63b
ethanburmane <ethanburmane@gmail.com>
2025-02-05 17:45:25 -0500

    working playlist import from spotify to local database

---
+++ src/main/utils/spotify.js
@@ -0,0 +1,96 @@
+const axios = require('axios');
+const sqlite3 = require('sqlite3');
+
+var COLLECTION_URL = 'https://open.spotify.com/playlist/5yal4glZg3isyuhkLenHeT';
+
+class SpotifyApi {
+
+    constructor() {
+        this.token = null;
+    }
+
+    async initialize() {
+        this.token = await SpotifyApi.getToken();
+        if (!this.token) {
+            throw new Error('Failed to get token');
+        }
+    }    
+
+    static async getToken() {
+
+        if (!process.env.CLIENT_SECRET) {
+            throw new Error('Must set CLIENT_SECRET environment variable');
+        }
+
+        const params = new URLSearchParams();
+        params.append('grant_type', 'client_credentials');
+        params.append('client_id', '3852c03c669e46dd93e28ee6d4bd15c4');
+        params.append('client_secret', process.env.CLIENT_SECRET);
+
+        const response = await axios.post('https://accounts.spotify.com/api/token', params, {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
+
+        return response.data.access_token;
+    }
+
+    async getPlaylist(url) {
+
+        if (!this.token) { 
+            await this.initialize();
+        }
+
+        if (url.search('playlist') == -1) {
+            throw new Error('Invalid playlist URL');
+        }
+
+        let collection_id = url.split('playlist/')[1];
+
+        // TODO: get playlist id from user input (or other source)
+        const response = axios.get(`https://api.spotify.com/v1/playlists/${collection_id}`, {
+            headers: { 'Authorization': `Bearer ${this.token}` }
+        })
+        .then((response) => {      
+            this.storePlaylist(response.data); // store playlist
+        }, (error) => {
+            console.log(error);
+        });
+    }
+
+    /* 
+    Stores the imported music from Spotify into its respective table in the database:
+    - each playlist will have its own table on the playlists database
+    - each album will have its own table on the albums database
+    */
+    async storePlaylist(musicObj) {
+        var db = null;
+        var table_name = musicObj.name;
+
+        db = new sqlite3.Database(`../db/playlists.db`, (err) => {
+            if (err) {
+                console.error(err.message);
+            }
+        });
+
+        // validate and fix table name 
+        // TODO: add check for reserved keywords like SELECT or INSERT
+        table_name = table_name.replace(/[^a-zA-Z0-9]/g, '_');
+
+        db.serialize(() => {
+            db.run(`CREATE TABLE IF NOT EXISTS ${table_name} (name TEXT, album TEXT, artist TEXT, uri TEXT, duration INTEGER)`);
+        });
+
+        musicObj.tracks.items.forEach((item) => {
+            db.run(`INSERT INTO ${table_name} (name, album, artist, uri, duration) VALUES (?, ?, ?, ?, ?)`, [item.track.name, item.track.album.name, item.track.artists[0].name, item.track.uri, item.track.duration_ms], (err) => {
+                if (err) {
+                    console.error(err.message);
+                }
+            });
+        });
+    }
+}
+
+let spotify = new SpotifyApi();
+spotify.getPlaylist(COLLECTION_URL);
