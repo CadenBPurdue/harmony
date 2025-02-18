e774e08254d0ec12fb20734392962d4ba94fb65f
GitHub Action <action@github.com>
2025-02-18 20:24:17 +0000

    Auto-format code using ESlint

--- src/main/utils/firebase.js
+++ src/main/utils/firebase.js
@@ -1,5 +1,5 @@
-import { fileURLToPath } from "url";
 import path from "path";
+import { fileURLToPath } from "url";
 import dotenv from "dotenv";
 import { app } from "electron";
 import { initializeApp } from "firebase/app";
