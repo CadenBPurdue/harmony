a7655ca565978df242d94b41aaf36e05737225d9
Icyadam14 <adamkahl8@gmail.com>
2025-02-19 18:36:06 -0500

    Remove react strict mode

--- src/renderer/main.jsx
+++ src/renderer/main.jsx
@@ -65,7 +65,5 @@ const Router = () => {
 // Create root and render
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Router />
-  </React.StrictMode>,
+  <Router />
 );
